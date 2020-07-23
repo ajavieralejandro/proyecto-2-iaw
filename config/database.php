4367c29092545f7006dcb3fc13633c6aa758ca37
ajavieralejandro <amorosijavier@gmail.com>
2020-07-23 15:02:49 -0300

    database config

--- config/database.php
+++ config/database.php
@@ -62,6 +62,19 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        /*
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'require',
+        ],*/
 
         'pgsql' => [
             'driver' => 'pgsql',
