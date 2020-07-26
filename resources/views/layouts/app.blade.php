1a256f9656c3e58fede39566db619fbdb3aa6ff9
ajavieralejandro <amorosijavier@gmail.com>
2020-07-26 20:31:29 -0300

    api token meta added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @if(Auth::user())
+    <meta name="api_token" content="{{ Auth::user()->api_token }}">
+    @endif
+
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
