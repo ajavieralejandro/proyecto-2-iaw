b0e3d3fcd389010370832b44a200c629515c3b09
ajavieralejandro <amorosijavier@gmail.com>
2020-07-30 00:14:46 -0300

    blade changes for Curso form

--- resources/views/curso/addnewcurso.blade.php
+++ resources/views/curso/addnewcurso.blade.php
@@ -28,25 +28,25 @@
 
   <div class="form-group">
   <label for="input1">Nombre del Curso: </label>
-    <input required  type="text" class="form-control" id="input1" name="name"  placeholder="Ej : Biomodelos">
+    <input required  type="text" class="form-control" value="{{ old('name) }}" id="input1" name="name"  placeholder="Ej : Biomodelos">
   </div>
   <div class="form-group">
   <label for="input2">Youtube Link : </label>
-    <input required  type="text" class="form-control" name="youtubelink" id="input2"  placeholder="https://youtube.com">
+    <input required  type="text" class="form-control" value="{{ old('youtubelink') }}" name="youtubelink" id="input2"  placeholder="https://youtube.com">
   </div>
 
   <div class="form-group">
   <label for="input2">Classrom Link : </label>
-    <input required  type="text" name="link" class="form-control" id="input3" >
+    <input required  type="text" name="link" value="{{ old('link') }}" class="form-control" id="input3" >
   </div>
   <div class="form-group">
   <label for="input1">Precio: </label>
-    <input required  type="number" class="form-control" id="input0" name="price"  placeholder="500">
+    <input required  type="number" class="form-control" value="{{ old('price') }}" id="input0" name="price"  placeholder="500">
   </div>
 
   <div class="form-group">
   <label for="sel1">Selecciona el docente : </label>
-  <select required  class="form-control" name="docente" id="sel1">
+  <select required  class="form-control" value="{{ old('docente') }}" name="docente" id="sel1">
     @foreach ($docentes as $docente)
     <option value={{ $docente->id }} >{{$docente->name}}</option>
     @endforeach
@@ -56,7 +56,7 @@
 
   <div class="form-group">
   <label for="comment">Descripcion</label>
-  <textarea required  name="descripcion" class="form-control" rows="5" id="comment"></textarea>
+  <textarea required  name="descripcion" value="{{ old('description') }}" class="form-control" rows="5" id="comment"></textarea>
 </div>
 
 <div class="image-upload" style="text-align:center">
