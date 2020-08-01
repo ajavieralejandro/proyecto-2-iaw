5f3457d89423d7e8a69b98d396425c6f5fb9174d
ajavieralejandro <amorosijavier@gmail.com>
2020-07-31 22:42:36 -0300

    buildpack deploy

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import SearchComponent from "./Search/search.component";
 const token = document.getElementById("api_token").content;
-
+//buildpack deploy
 function Example() {
     useEffect(() => {
         console.log("Estoy trayendo : ", token);
