0cfb2bb11b701e82eaaf74ac212acbb76fb2afb9
ajavieralejandro <amorosijavier@gmail.com>
2020-08-01 21:34:42 -0300

    database fix

--- config/passport.php
+++ config/passport.php
@@ -59,7 +59,7 @@ return [
 
     'storage' => [
         'database' => [
-            'connection' => env('DB_CONNECTION', 'psgql'),
+            'connection' => env('DB_CONNECTION', 'pgsql'),
         ],
     ],
 
